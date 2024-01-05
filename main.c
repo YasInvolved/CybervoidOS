922646b8d4eb31bb079c0bcf32cdf05ebd3d47db
YasInvolved <szrtxm.op@gmail.com>
2024-01-05 18:49:06 +0100

    Fix recent errors

--- main.c
+++ main.c
@@ -86,6 +86,25 @@ INT32 GetCursorCol() {
     return ST->ConOut->Mode->CursorColumn;
 }
 
+EFI_STATUS PrintInfo(CHAR16* Message) {
+    Print(L"[INFO] %s\n", Message);
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS PrintError(CHAR16* Message) {
+    SetTextColor(EFI_RED, EFI_BACKGROUND_BLACK);
+    Print(L"[ERROR] %s\n", Message);
+    SetTextColor(EFI_WHITE, EFI_BACKGROUND_BLACK);
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS PrintWarn(CHAR16* Message) {
+    SetTextColor(EFI_YELLOW, EFI_BACKGROUND_BLACK);
+    Print(L"[WARN] %s\n", Message);
+    SetTextColor(EFI_WHITE, EFI_BACKGROUND_BLACK);
+    return EFI_SUCCESS;
+}
+
 /*
     Console input related functions
 */
@@ -115,42 +134,49 @@ EFI_STATUS InitializeFS() {
     status = uefi_call_wrapper(gBS->HandleProtocol, 3, IH, &gEfiLoadedImageProtocolGuid, (void**)&LoadedImage);
     if (EFI_ERROR(status)) {
         SetTextColor(EFI_RED, EFI_BACKGROUND_BLACK);
-        Print(L"Error: Could not set up filesystem.\n");
+        PrintError(L"Could not load image.");
         SetTextColor(EFI_WHITE, EFI_BACKGROUND_BLACK);
         return status;
     }
     
     EFI_DEVICE_PATH_PROTOCOL *DevicePath;
-    status = uefi_call_wrapper(gBS->HandleProtocol, 3, IH, &gEfiDevicePathProtocolGuid, (void**)&DevicePath);
+    EFI_GUID DevicePathProtocolGUID = EFI_DEVICE_PATH_PROTOCOL_GUID;
+    status = uefi_call_wrapper(gBS->HandleProtocol, 3, LoadedImage->DeviceHandle, &DevicePathProtocolGUID, (void**)&DevicePath);
     if (EFI_ERROR(status)) {
-        CHAR16* fmt;
-        StatusToString(fmt, status);
-        SetTextColor(EFI_RED, EFI_BACKGROUND_BLACK);
-        Print(L"Error: Could not set up filesystem. - %s\n", fmt);
-        SetTextColor(EFI_WHITE, EFI_BACKGROUND_BLACK);
+        PrintError(L"Could not set up filesystem.");
         return status;
     }
 
-    status = uefi_call_wrapper(gBS->HandleProtocol, 3, IH, &gEfiSimpleFileSystemProtocolGuid, (void**)&Volume);
+    EFI_GUID SimpleFilesystemProtocolGUID = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+    status = uefi_call_wrapper(gBS->HandleProtocol, 3, LoadedImage->DeviceHandle, &SimpleFilesystemProtocolGUID, (void**)&Volume);
     if (EFI_ERROR(status)) {
-        CHAR16* fmt;
-        StatusToString(fmt, status);
-        SetTextColor(EFI_RED, EFI_BACKGROUND_BLACK);
-        Print(L"Error: Could not set up filesystem. - %s\n", fmt);
-        SetTextColor(EFI_WHITE, EFI_BACKGROUND_BLACK);
+        PrintError(L"Could not set up filesystem.");
         return status;
     }
+    return EFI_SUCCESS;
 }
 EFI_FILE_PROTOCOL* openFile(CHAR16* FileName) {
     EFI_STATUS status;
-    Print(L"Opening %s...\n", FileName);
+    Print(L"[INFO] Opening %s...\n", FileName);
     EFI_FILE_PROTOCOL* RootFS;
     status = uefi_call_wrapper(Volume->OpenVolume, 2, Volume, &RootFS);
-    if (EFI_ERROR(status)) Print(L"Error: failed to open file: %s\n", FileName);
+    if (EFI_ERROR(status)) {
+        CHAR16* fmt;
+        SetTextColor(EFI_RED, EFI_BACKGROUND_BLACK);
+        StatusToString(fmt, status);
+        Print(L"[ERROR] Failed to open file %s: %s\n", FileName, fmt);
+        SetTextColor(EFI_WHITE, EFI_BACKGROUND_BLACK);
+    }
 
     EFI_FILE_PROTOCOL* FileHandle = NULL;
     status = RootFS->Open(RootFS, &FileHandle, FileName, 0x0000000000000001, 0);
-    if (EFI_ERROR(status)) Print(L"Error: failed to open file: %s\n", FileName);
+    if (EFI_ERROR(status)) {
+        CHAR16* fmt;
+        SetTextColor(EFI_RED, EFI_BACKGROUND_BLACK);
+        StatusToString(fmt, status);
+        Print(L"[ERROR] Failed to open file %s: %s\n", FileName, fmt);
+        SetTextColor(EFI_WHITE, EFI_BACKGROUND_BLACK);
+    }
 
     return FileHandle;
 }
@@ -164,18 +190,17 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     ClearSceen();
     InitGop();
     SetTextColor(EFI_GREEN, EFI_BACKGROUND_BLACK);
-    Print(L"Welcome to CybervoidOS!\n");
+    Print(L"Welcome to CybervoidOS!\n\n");
     SetTextColor(EFI_WHITE, EFI_BACKGROUND_BLACK);
-    Print(L"Initializing fs...\n");
+    PrintInfo(L"Initializing fs...");
     EFI_STATUS s = InitializeFS();
     if(EFI_ERROR(s)) {
         CHAR16* fmt;
         StatusToString(fmt, s);
-        Print(L"PANIC: %s\n", fmt);
+        Print(L"[CRITICAL]: Couldn't initialize file system. Abort.\n", fmt);
         ST->BootServices->Stall(10000);
         ST->RuntimeServices->ResetSystem(EfiResetCold, EFI_SUCCESS, 0, 0);
     }
-
     // test (temporary)
     void* OSBuffer_Handle;
     UINT64 fileSize = 0x00100000;
@@ -190,6 +215,7 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     while ((uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2, ST->ConIn, &Key)) == EFI_NOT_READY);
     if (Key.UnicodeChar == L's') ST->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, 0);
     if (Key.UnicodeChar == L'r') ST->RuntimeServices->ResetSystem(EfiResetWarm, EFI_SUCCESS, 0, 0);
-   // Shutdown system after kernel return ST->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, 0);
+    // Shutdown system after kernel return 
+    ST->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, 0);
     return EFI_SUCCESS;
 }
